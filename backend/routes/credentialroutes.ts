4d73fccda27294736eda5ab3d899390fbeb0e43c
bnspopi <sinhashounak@gmail.com>
2026-03-05 00:08:19 +0530

    Create credentialroutes.ts

---
+++ backend/routes/credentialroutes.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono';
+import * as credentialController from '../controllers/credential.controller.ts';
+import { authMiddleware } from '../middlewares/authMiddleware.ts';
+
+
+const credentialRoutes = new Hono();
+
+credentialRoutes.use('*', authMiddleware);
+
+credentialRoutes.get('/', credentialController.getCredentials);
+credentialRoutes.post('/', credentialController.issueCredential);
+credentialRoutes.post('/eligibility', credentialController.issueEligibilityCredential);
+
+export default credentialRoutes;
