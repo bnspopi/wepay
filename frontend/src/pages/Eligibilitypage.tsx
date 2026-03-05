c686977890eac9a4a625a1be976e95b8c84df58d
bnspopi <sinhashounak@gmail.com>
2026-03-05 14:07:55 +0530

    Create Eligibilitypage.tsx

---
+++ frontend/src/pages/Eligibilitypage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function EligibilityPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Eligibility Check</h1>
+      <p>Check if you qualify for welfare schemes</p>
+    </div>
+  );
+}
+
+export default EligibilityPage;
