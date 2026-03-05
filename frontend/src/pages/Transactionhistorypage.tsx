1b7f9d7e86751c221bf592fb0e807ec2a4073d67
bnspopi <sinhashounak@gmail.com>
2026-03-05 14:08:50 +0530

    Create Transactionhistorypage.tsx

---
+++ frontend/src/pages/Transactionhistorypage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TransactionHistoryPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Transaction History</h1>
+      <p>View all past transactions</p>
+    </div>
+  );
+}
+
+export default TransactionHistoryPage;
