6363cd500c608bd3e3a12476fbb9ae8ffdae9566
bnspopi <sinhashounak@gmail.com>
2026-03-05 14:08:24 +0530

    Create Transactionpage.tsx

---
+++ frontend/src/pages/Transactionpage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TransactionPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Transaction Page</h1>
+      <p>Send or receive welfare funds</p>
+    </div>
+  );
+}
+
+export default TransactionPage;
