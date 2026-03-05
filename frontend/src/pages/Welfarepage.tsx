86dad7c386296b5d160a10e067b16f957ae038b6
bnspopi <sinhashounak@gmail.com>
2026-03-05 14:09:11 +0530

    Create Welfarepage.tsx

---
+++ frontend/src/pages/Welfarepage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function WelfarePage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welfare Dashboard</h1>
+      <p>Overview of welfare benefits</p>
+    </div>
+  );
+}
+
+export default WelfarePage;
