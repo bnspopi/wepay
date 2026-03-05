5d323b00ae0ee2bb13356e412278cb2034edae05
bnspopi <sinhashounak@gmail.com>
2026-03-05 14:05:54 +0530

    Create Welfareschemespage.tsx

---
+++ frontend/src/pages/Welfareschemespage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function WelfareSchemesPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welfare Schemes</h1>
+      <p>Available government welfare schemes</p>
+    </div>
+  );
+}
+
+export default WelfareSchemesPage;
