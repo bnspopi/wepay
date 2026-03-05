40eae2659433a1501754e9cd930798d017e465de
bnspopi <sinhashounak@gmail.com>
2026-03-05 14:07:35 +0530

    Create Registerpage.tsx

---
+++ frontend/src/pages/Registerpage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RegisterPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Register Page</h1>
+      <p>Create your account</p>
+    </div>
+  );
+}
+
+export default RegisterPage;
