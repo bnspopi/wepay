805d73743d6f133a24a8eb49b3fd7bfe68bf4418
bnspopi <sinhashounak@gmail.com>
2026-03-05 14:07:12 +0530

    Create Loginpage.tsx

---
+++ frontend/src/pages/Loginpage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Login Page</h1>
+      <p>User login portal</p>
+    </div>
+  );
+}
+
+export default LoginPage;
