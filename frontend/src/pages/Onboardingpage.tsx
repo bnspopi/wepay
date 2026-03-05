e35442e2c9e3d70a95977a199e04f603a2cea958
bnspopi <sinhashounak@gmail.com>
2026-03-05 14:06:52 +0530

    Create Onboardingpage.tsx

---
+++ frontend/src/pages/Onboardingpage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function OnboardingPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>User Onboarding</h1>
+      <p>Complete your onboarding process</p>
+    </div>
+  );
+}
+
+export default OnboardingPage;
