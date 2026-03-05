af2fa020af370764dd7f19607132b52ff54c144a
bnspopi <sinhashounak@gmail.com>
2026-03-05 15:18:34 +0530

    Update LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+    alert("Login Successful");
+  };
+
   return (
     <div style={{ padding: 20 }}>
       <h1>Login Page</h1>
-      <p>User login portal</p>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
